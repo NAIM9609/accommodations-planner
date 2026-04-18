3f4a5388a2298407ee56b8d82a101c2d670616bd
NaimH <samuel.haddar96@gmail.com>
2026-04-18 16:12:01 +0200

    feat: Add nested CloudFormation stacks for DynamoDB, IAM, and Lambda

--- infrastructure/modules/api_gateway/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "api_url" {
-  value = aws_apigatewayv2_stage.api.invoke_url
-}
-
-output "api_id" {
-  value = aws_apigatewayv2_api.api.id
-}
--- infrastructure/modules/dynamodb/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "table_arn" {
-  value = aws_dynamodb_table.reservations.arn
-}
-
-output "table_name" {
-  value = aws_dynamodb_table.reservations.name
-}
